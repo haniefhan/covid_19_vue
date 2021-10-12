a8c0cbdaa90045ecd6be0e3aee354e0609f5ef75
Hanief Han <haniefhan@gmail.com>
2021-10-12 18:11:42 +0700

    vue :: redesign toptencovidcases using tailwindcss

--- src/views/TopTenCovidCases.vue
+++ src/views/TopTenCovidCases.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
-    <center>
-      <h1>{{ title }}</h1>
-      <table id="topten">
+  <div class="mt-16 p-6 mx-auto max-w-3xl">
+    <h1 class="text-3xl text-center text-red-400 font-semibold">{{ title }}</h1>
+    <div class="mt-4 bg-white rounded-lg shadow-md p-6">
+      <table id="topten" class="table-auto border-collapse w-full">
         <thead>
-          <th>Rank</th>
-          <th>Country Name</th>
-          <th>Total Cases</th>
-          <th>New Cases</th>
-          <th>New Deaths</th>
-          <th>Total Deaths</th>
-          <th>Total Recovered</th>
+          <tr class="text-sm font-medium text-gray-700 text-center">
+            <th class="px-2 py-2 bg-red-300">Rank</th>
+            <th class="px-2 py-2 bg-red-300">Country Name</th>
+            <th class="px-2 py-2 bg-red-300">Total Cases</th>
+            <th class="px-2 py-2 bg-red-300">New Cases</th>
+            <th class="px-2 py-2 bg-red-300">New Deaths</th>
+            <th class="px-2 py-2 bg-red-300">Total Deaths</th>
+            <th class="px-2 py-2 bg-red-300">Total Recovered</th>
+          </tr>
         </thead>
         <tbody>
-          <tr v-for="data in topcases" :key="data.country_rank">
-            <td>{{ data.country_rank }}</td>
-            <td>{{ data.country_name }}</td>
-            <td>{{ data.total_cases | formatNumber }}</td>
-            <td>{{ data.new_cases  | formatNumber }}</td>
-            <td>{{ data.new_deaths  | formatNumber }}</td>
-            <td>{{ data.total_deaths  | formatNumber }}</td>
-            <td>{{ data.total_recovered  | formatNumber }}</td>
+          <tr v-for="data in topcases" :key="data.country_rank" class="hover:bg-red-50 text-sm border-b border-red-300">
+            <td class="px-2 py-3">{{ data.country_rank }}</td>
+            <td class="px-2 py-3">{{ data.country_name }}</td>
+            <td class="px-2 py-3">{{ data.total_cases | formatNumber }}</td>
+            <td class="px-2 py-3 text-right">{{ data.new_cases | formatNumber }}</td>
+            <td class="px-2 py-3 text-right">{{ data.new_deaths | formatNumber }}</td>
+            <td class="px-2 py-3 text-right">{{ data.total_deaths | formatNumber }}</td>
+            <td class="px-2 py-3 text-right">{{ data.total_recovered | formatNumber }}</td>
           </tr>
         </tbody>
       </table>
-    </center>
+    </div>
   </div>
 </template>
 
@@ -58,21 +60,3 @@ export default {
   },
 };
 </script>
-
-<style>
-table#topten {
-  border-collapse: collapse;
-  width: 90%;
-}
-table#topten td, table#topten th {
-  border: 1px solid black;
-  padding: 5px;
-  text-align: right;
-}
-table#topten th {
-  text-align: center;
-}
-table#topten td:nth-child(2) {
-  text-align: left;
-}
-</style>
